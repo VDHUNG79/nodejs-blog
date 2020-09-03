b8b2900cc076bae8de031bc7e6d9352936e47e95
VDHUNG79 <64667768+VDHUNG79@users.noreply.github.com>
2020-09-03 18:52:49 +0700

    routes and controllers

---
+++ 17-mvc/src/index.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars  = require('express-handlebars');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+// HTTP Logger
+// app.use(morgan('combined'));
+
+//  Template Engine
+app.engine('hbs', handlebars({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+console.log('PATH: ', path.join(__dirname, 'resources/views'));
+
+// Route
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/news', (req, res) => {
+    res.render('news');
+});
+
+app.get('/search', (req, res) => {
+    // console.log(req.query.q);
+    res.render('search');
+});
+
+app.post('/search', (req, res) => {
+    console.log(req.body);
+    res.send(''); // send('') chuỗi rỗng sẽ gửi đi màn hình rỗng.
+});
+
+// Local host
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
