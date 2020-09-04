86872a9b074da997692923e079f5ad5401dad6c8
VDHUNG79 <64667768+VDHUNG79@users.noreply.github.com>
2020-09-04 11:11:10 +0700

    JSON Viewer

---
+++ 22-json-viewer/src/routes/index.js
@@ -0,0 +1,30 @@
+const newsRouter = require('./news')
+const siteRouter = require('./site')
+
+function route(app) {
+
+    // Route (Move from index.js)
+
+    app.use('/news', newsRouter);
+    // app.get('/news', (req, res) => {
+    //     res.render('news');
+    // });
+
+    app.use('/', siteRouter);
+    // app.get('/', (req, res) => {
+    //     res.render('home');
+    // });
+
+    // app.get('/search', (req, res) => {
+        // console.log(req.query.q);
+    //     res.render('search');
+    // });
+
+    // app.post('/search', (req, res) => {
+    //     console.log(req.body);
+    //     res.send(''); // send('') chuỗi rỗng sẽ gửi đi màn hình rỗng.
+    // });
+
+}
+
+module.exports = route;
