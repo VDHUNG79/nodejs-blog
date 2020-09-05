e7cca9f9852bb9383dc74c1a0eec0aeafa5b088f
VDHUNG79 <64667768+VDHUNG79@users.noreply.github.com>
2020-09-05 08:49:21 +0700

    create courses

---
+++ 28-crud-update-course/src/config/db/index.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+async function connect() {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/f8_express_tutorial', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        // useFindAndModify: false,
+        // useCreateIndex: true
+        });
+        console.log('Connect successfully!!!');
+    } catch (error) {
+        console.log('Connect failure!!!');
+    }
+}
+
+module.exports = { connect }; 
+// Export {} là Object nên const db sẽ là Object
