527107b440de5194891c3be8e53d38fb6aed9845
VDHUNG79 <64667768+VDHUNG79@users.noreply.github.com>
2020-09-06 21:36:37 +0700

    soft delete courses

---
+++ 30-soft-delete/src/app/models/Course.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const slug = require('mongoose-slug-generator');
+
+const mongooseDelete = require('mongoose-delete');
+
+
+const Schema = mongoose.Schema;
+
+const Course = new Schema ({
+    name: { type: String, required: true },
+    description: { type: String },
+    image: { type: String },
+    videoId: { type: String, required: true },
+    level: { type: String },
+    slug: { type: String, slug: 'name', unique: true },
+    // createdAt: { type: Date, default: Date.now },
+    // updateAt: { type: Date, default: Date.now }
+
+}, {
+    timestamps: true,
+});
+
+// Add plugins
+mongoose.plugin(slug);
+Course.plugin(mongooseDelete, {
+    deletedAt : true,
+    overrideMethods: 'all'
+});
+
+module.exports = mongoose.model('Course', Course);
+
+//  Read more about index in MongoDB and Database
